584710d83b7d9aa087a36eb95642bd49dfb9774c
itsalitee <ali.tohidi@sjsu.edu>
2023-12-18 09:55:06 -0800

    directory structure added, main test is added

---
+++ main.py
@@ -0,0 +1,3 @@
+from utils.directory_structure import ASSETS, DATA_DIR
+
+print(DATA_DIR, ASSETS)
---
+++ utils/directory_structure.py
@@ -0,0 +1,18 @@
+import os
+
+# directory structure
+CU_DIR = os.path.curdir
+PROJ_DIR = os.path.join(CU_DIR, "..")
+PROJ_DIR = os.path.abspath(PROJ_DIR)
+
+# normalized paths
+DATA_DIR = os.path.join(PROJ_DIR, "data")
+OUTPUT_DIR = os.path.join(PROJ_DIR, "outputs")
+ASSETS = os.path.join(PROJ_DIR, "assets")
+RESOURCES = os.path.join(PROJ_DIR, "resources")
+
+dir_list = [DATA_DIR, OUTPUT_DIR, ASSETS, RESOURCES]
+
+for d in dir_list:
+    if not os.path.exists(os.path.join(PROJ_DIR, d)):
+        os.makedirs(d, exist_ok=True)
